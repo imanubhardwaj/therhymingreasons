0c21a46e5d70e5b03b376b51f64af8ab101dbe6d
Manu Bhardwaj <manu.bhardwaj@instasolv.com>
2020-06-08 22:30:16 +0530

    [update] api base url

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -2,6 +2,7 @@ const withCSS = require("@zeit/next-css");
 module.exports = withCSS({
     distDir: "../../dist/client",
     env: {
-        BASE_URL: "https://therhymingreasons.com"
+        BASE_URL: "https://therhymingreasons.com",
+        API_BASE_URL: "https://therhymingreasons.herokuapp.com/api/v1"
     }
 });
--- src/client/services/api.js
+++ src/client/services/api.js
@@ -3,24 +3,38 @@ import CancelablePromise from "../utils/cancelable-promise";
 
 class ApiService {
     constructor() {
-        axios.defaults.baseURL = "https://therhymingreasons.herokuapp.com/api/v1"
+        axios.defaults.baseURL = process.env.API_BASE_URL
     }
 
     getPosts() {
         return new CancelablePromise((resolve, reject) => {
-            axios.get("posts").then(res => resolve(res.data.data), error => reject(error.message));
+            axios.get("posts")
+                .then(res => resolve(res.data.data))
+                .catch(error => reject(error.message));
+        });
+    }
+
+    searchPosts(query) {
+        return new CancelablePromise((resolve, reject) => {
+            axios.get(`search?query=${query}`)
+                .then(res => resolve(res.data.data))
+                .catch(error => reject(error.message));
         });
     }
 
     getPost(slug) {
         return new CancelablePromise((resolve, reject) => {
-            axios.get(`posts/${slug}`).then(res => resolve(res.data.data), error => reject(error.message));
+            axios.get(`posts/${slug}`)
+                .then(res => resolve(res.data.data))
+                .catch(error => reject(error.message));
         });
     }
 
     postComment(slug, data) {
         return new CancelablePromise((resolve, reject) => {
-            axios.post(`posts/${slug}/comment`, data).then(res => resolve(res.data.data), error => reject(error.message));
+            axios.post(`posts/${slug}/comment`, data)
+                .then(res => resolve(res.data.data))
+                .catch(error => reject(error.message));
         })
     }
 }
